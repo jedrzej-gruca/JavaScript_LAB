82387ad8455cebf6e6d2993002844c9c994af224
jedrzej-gruca <jedrzej.gruca@gmail.com>
2023-01-26 21:15:45 +0100

    created new slider using html css and js for LAB2

--- LAB2/main.js
+++ LAB2/main.js
@@ -1,24 +1,79 @@
-// notatnik z zajęć
-const main = document.querySelector('main')
-const timeoutRef = setTimeout( 
-    () => {
-        main.innerHTML='From setTimeout'
-    },
-    2000
-)
-let licznik = 0 
-const intervalRef = setInterval( 
-    () => {
-        main.innerHTML='From interval' + licznik++
-    },
-    4000
-)
+function autoSlide() {
+    setInterval(() => {
+        slide(getActiveSlideIndex() + 1);
+    }, 5000);
+}
 
-// kasujemy setInterval
-clearInterval(intervalRef)
+window.addEventListener("load", () => {
+    autoSlide();
 
-// kasujemy setTimeout
-clearTimeout(intervalRef)
+    const dots = document.querySelectorAll(".dot");
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("click", () => slide(i));
+    }
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+    prevButton.addEventListener("click", () => slide(getActiveSlideIndex() - 1));
+    nextButton.addEventListener("click", () => slide(getActiveSlideIndex() + 1));
+})
 
+let isCurrentSlide = false;
 
-// window.requestAnimationFrame
+function slide(toIndex) {
+    if (isCurrentSlide)
+        return;
+    isCurrentSlide = true;
+
+    const slides = Array.from(document.querySelectorAll(".slide"));
+    const activeSlide = document.querySelector(".activeSlide");
+    const activeSlideIndex = slides.indexOf(activeSlide);
+    let newActiveSlide = null;
+
+    if (toIndex > activeSlideIndex) {
+        if (toIndex >= slides.length) {
+            toIndex = 0;
+        }
+
+        newActiveSlide = slides[toIndex];
+        newActiveSlide.classList.add("nextPosition");
+        setTimeout(() => {
+            newActiveSlide.classList.add("nextSlide");
+            activeSlide.classList.add("nextSlide");
+        }, 20);
+    } else {
+        if (toIndex < 0) {
+            toIndex = slides.length - 1;
+        }
+
+        newActiveSlide = slides[toIndex];
+        newActiveSlide.classList.add("prevPosition");
+        setTimeout(() => {
+            newActiveSlide.classList.add("prevSlide");
+            activeSlide.classList.add("prevSlide");
+        }, 20);
+    }
+
+    newActiveSlide.addEventListener("transitionend", () => {
+        activeSlide.className = "slide";
+        newActiveSlide.className = "slide activeSlide";
+        isCurrentSlide = false;
+    }, {
+        once: true
+    });
+    slideIndicator(toIndex);
+}
+
+function getActiveSlideIndex() {
+    const slides = Array.from(document.querySelectorAll(".slide"));
+    const activeSlide = document.querySelector(".activeSlide");
+    return slides.indexOf(activeSlide);
+}
+
+function slideIndicator(toIndex) {
+    const dots = document.querySelectorAll(".dot");
+    const dotActive = document.querySelector(".activeDot");
+    const newDotActive = dots[toIndex];
+
+    dotActive.classList.remove("activeDot");
+    newDotActive.classList.add("activeDot");
+}
