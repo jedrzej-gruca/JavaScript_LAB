18258b148cfdd140f48b7091ed91b93c5555dd73
jedrzej-gruca <jedrzej.gruca@gmail.com>
2023-01-29 10:22:14 +0100

    clear the code from commented lines

--- LAB3/drumkit.js
+++ LAB3/drumkit.js
@@ -4,16 +4,10 @@ document.querySelector('#play2Button').addEventListener('click', onPlayChannel2B
 document.querySelector('#play3Button').addEventListener('click', onPlayChannel3ButtonClick);
 document.querySelector('#play4Button').addEventListener('click', onPlayChannel4ButtonClick);
 document.querySelector('#playAllButton').addEventListener('click',onPlayAllButtonClick);
-// document.querySelector('#recButton').addEventListener('click', onRecButtonClick);
 document.querySelector('#recOnChannel1').addEventListener('click',onRecOnChannel1ButtonClick);
 document.querySelector('#recOnChannel2').addEventListener('click',onRecOnChannel2ButtonClick);
 document.querySelector('#recOnChannel3').addEventListener('click',onRecOnChannel3ButtonClick);
 document.querySelector('#recOnChannel4').addEventListener('click',onRecOnChannel4ButtonClick);
-// document.querySelector('#channel1').addEventListener('click',switchChannelTo1);
-// document.querySelector('#channel2').addEventListener('click',switchChannelTo2);
-// document.querySelector('#channel3').addEventListener('click',switchChannelTo3);
-// document.querySelector('#channel4').addEventListener('click',switchChannelTo4);
-
 
 //tabs with recorded sounds
 const recordedSound1 = [];
@@ -103,38 +97,6 @@ function playSound(sound) {
     pressedKey.currentTime = 0;
     pressedKey.play();
 }
-
-// function onRecButtonClick() {
-//     recStartTime = Date.now();
-// }
-
-// function onPlayButtonClick() {
-//     let recordedSounds = [];
-//     recordedSounds.length = 0;
-//     switch(selectedChannel){
-//         case 1:
-//             recordedSounds = recordedSound1;
-//             break;
-//         case 2:
-//             recordedSounds = recordedSound2;
-//             break;
-//         case 3:
-//             recordedSounds = recordedSound3;
-//             break;
-//         case 4:
-//             recordedSounds = recordedSound4;
-//             break;
-//     }
-//     for (let i = 0; i < recordedSounds.length; i++) {
-//         const soundObj = recordedSounds[i];
-//         setTimeout(
-//             () => {
-//                 playSound(soundObj.id);
-//             },
-//             soundObj.time
-//         );
-//     }
-// }
 
 function onPlayChannel1ButtonClick() {
     for (let i = 0; i < recordedSound1.length; i++) {
